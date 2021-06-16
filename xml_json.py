c337a0a58fc41f0658069f147d764abe222547fb
Nataliia.Kolesnyk <kolesnyk.nataly@gmail.com>
2021-06-16 17:47:47 +0300

    add functions and option to select filter

--- xml_json.py
+++ xml_json.py
@@ -1,26 +1,59 @@
 import json
-import xmltodict as xmltodict
+import sys
 
-with open('dataset.xml', encoding='utf-8') as xml_file:
-    data_dict = xmltodict.parse(xml_file.read())
-    xml_file.close()
+import xmltodict
 
-    filtered_records = []
-    for record in data_dict['dataset']['record']:
-        if record['score'] is not None and int(record['score']) > 500 and record['gender'] == 'Female':
-            filtered_records.append(record)
+utf8_string = 'utf-8'
 
-    filtered_records_with_root = {'dataset': {'record': filtered_records}}
-    json_data = json.dumps(filtered_records_with_root, indent=4, ensure_ascii=False)
+file_path_argument = sys.argv[1]
+filter_argument = None if len(sys.argv) < 3 else sys.argv[2].replace("-", "")
 
-    with open('dataset.json', 'w', encoding='utf-8') as json_file:
-        json_file.write(json_data)
-        json_file.close()
 
-with open('dataset.json', encoding='utf-8') as json_file2:
-    json_data2 = json.load(json_file2)
-    data_dict2 = xmltodict.unparse(json_data2, pretty=True)
+def xml_to_json():
+    with open(file_path_argument, encoding=utf8_string) as xml_file:
+        if filter_argument is None:
+            data_dict = xmltodict.parse(xml_file.read())
+            json_data = json.dumps(data_dict, indent=4, ensure_ascii=False)
+            with open('dataset_output.json', 'w', encoding=utf8_string) as output_json_file:
+                output_json_file.write(json_data)
+        elif filter_argument == "f":
+            data_dict = xmltodict.parse(xml_file.read())
+            filtered_records = []
+            for record in data_dict['dataset']['record']:
+                if record['score'] is not None and int(record['score']) > 500 and record['gender'] == 'Female':
+                    filtered_records.append(record)
+            filtered_records_with_root = {'dataset': {'record': filtered_records}}
+            json_data = json.dumps(filtered_records_with_root, indent=4, ensure_ascii=False)
+            with open('filtered_dataset_output.json', 'w', encoding=utf8_string) as output_json_file:
+                output_json_file.write(json_data)
+        else:
+            print('Invalid filter argument: ' + filter_argument)
 
-    with open('NEWdataset.xml', 'w', encoding='utf-8') as new_xml_file:
-        new_xml_file.write(data_dict2)
-        new_xml_file.close()
+
+def json_to_xml():
+    with open(file_path_argument, encoding=utf8_string) as json_file:
+        if filter_argument is None:
+            json_data = json.load(json_file)
+            data_dict = xmltodict.unparse(json_data, pretty=True)
+            with open('dataset_output.xml', 'w', encoding=utf8_string) as output_xml_file:
+                output_xml_file.write(data_dict)
+        elif filter_argument == "f":
+            json_data = json.load(json_file)
+            filtered_records = []
+            for record in json_data['dataset']['record']:
+                if record['score'] is not None and int(record['score']) > 500 and record['gender'] == 'Female':
+                    filtered_records.append(record)
+            filtered_records_with_root = {'dataset': {'record': filtered_records}}
+            data_dict = xmltodict.unparse(filtered_records_with_root, pretty=True)
+            with open('filtered_dataset_output.xml', 'w', encoding=utf8_string) as output_xml_file:
+                output_xml_file.write(data_dict)
+        else:
+            print('Invalid filter argument: ' + filter_argument)
+
+
+if file_path_argument.lower().endswith('.xml'):
+    xml_to_json()
+elif file_path_argument.lower().endswith('.json'):
+    json_to_xml()
+else:
+    print('Invalid file extension')
